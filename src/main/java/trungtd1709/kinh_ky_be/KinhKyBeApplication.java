34eb92e6a25b45f4153f93606ae87ce87c4c01de
trungta17 <trung.ta@lge.com>
2025-10-09 01:35:53 +0700

    .

---
+++ src/main/java/trungtd1709/kinh_ky_be/KinhKyBeApplication.java
@@ -0,0 +1,14 @@
+package trungtd1709.kinh_ky_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinhKyBeApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        SpringApplication.run(KinhKyBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/trungtd1709/kinh_ky_be/KinhKyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package trungtd1709.kinh_ky_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinhKyBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
